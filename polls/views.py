07b2419911295da892a9fcdd926fcc788f23010d
MarkWhite-fvchat <belakovmark4@gmail.com>
2021-02-27 17:34:32 +0700

    ver.02

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
+    
+def meme(request):
+    return HttpResponse("<img scr='https://www.meme-arsenal.com/memes/4a8e6d4124c75f8de55e002c067c430e.jpg'>")
+ 
