73b15028acf4de456d0d6ffbdefbc57c5e3ed23f
MarkWhite-fvchat <belakovmark4@gmail.com>
2021-03-06 19:51:33 +0700

    ver.04

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text = models.CharField(max_length=500)
+    text = models.CharField(max_length=500, default="")
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.text
--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,23 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question, Choice
 
 def index(request):
-    q_all = Question.objects.all()
-    res = "<ol>"
-    for q in q_all:
-        res += "<li>%s</li>" % q.text
-    res += "</ol>"
-    return HttpResponse(res)
+    questions = Question.objects.all()
+    context = {
+        "questions" : questions
+    }
+    return render(request, "polls/index.html", context)
     
 def meme(request):
     return HttpResponse("<img src='https://www.meme-arsenal.com/memes/4a8e6d4124c75f8de55e002c067c430e.jpg'>")
  
 def detail(request, q_id):
-    res = "Question number %s." % q_id
-    return HttpResponse(res)
+    question = Question.objects.get(pk=q_id)
+    context ={
+        "question" : question
+    }
+    return render(request, "polls/detail.html", context)
 
 def results(request, q_id):
     res = "Result for question number %s." % q_id
