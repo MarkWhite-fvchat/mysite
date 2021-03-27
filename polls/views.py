d2cefd6002fb0225c312eb7e6206fc87c4d04b03
MarkWhite-fvchat <belakovmark4@gmail.com>
2021-03-27 13:02:31 +0300

    Last

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
-def index(request):
-    questions = Question.objects.all()
-    context = {
-        "questions" : questions
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "questions"
+
+    def get_queryset(self):
+        return Question.objects.all()
     
-def meme(request):
-    return HttpResponse("<img src='https://www.meme-arsenal.com/memes/4a8e6d4124c75f8de55e002c067c430e.jpg'>")
- 
-def detail(request, q_id):
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def results(request, q_id):
     question = Question.objects.get(pk=q_id)
     context ={
         "question" : question
     }
-    return render(request, "polls/detail.html", context)
+    return render(request, "polls/results.html", context)
 
 
 def vote(request, q_id):
@@ -31,9 +38,8 @@ def vote(request, q_id):
     return HttpResponseRedirect(reverse("polls:results", args=(q_id, )))
 
 
-def results(request, q_id):
-    question = Question.objects.get(pk=q_id)
-    context ={
-        "question" : question
-    }
-    return render(request, "polls/results.html", context)
+
+
+
+def meme(request):
+    return HttpResponse("<img src='https://www.meme-arsenal.com/memes/4a8e6d4124c75f8de55e002c067c430e.jpg'>")
